ee850de49013feef49ba13cdd758cc055b7b8652
Laevskiy <Laevskiy@mail.ru>
2024-10-17 22:23:48 +0300

    2-4 taska

--- src/HW/4/Input.tsx
+++ src/HW/4/Input.tsx
@@ -1,14 +1,15 @@
 type InputPropsType = {
-	currentText: any // НУЖНО ПРОТИПИЗИРОВАТЬ
-	setCurrentText: any // НУЖНО ПРОТИПИЗИРОВАТЬ
+	currentText: string // НУЖНО ПРОТИПИЗИРОВАТЬ
+	setCurrentText: (a:string)=>void// НУЖНО ПРОТИПИЗИРОВАТЬ
 };
 
 export const Input = (props: InputPropsType) => {
-	const onChangeHandler = (event: 'НУЖНО ПРОТИПИЗИРОВАТЬ') => {
+	const onChangeHandler = (event: string) => {
 		// НУЖНО ДОПИСАТЬ
+		props.setCurrentText(event)
 	};
 
-	// return (
-	//   <input id={'hw04-input'} type="text" value={props.currentText} onChange={onChangeHandler} />
-	// );
+	return (
+		<input id={'hw04-input'} type="text" value={props.currentText} onChange={(e)=>onChangeHandler(e.target.value)} />
+	);
 };
